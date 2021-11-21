831804e6ff9b5fa6fa749bd4df5e82c6fb4594ea
Bados2000 <mimi2222@vp.pl>
2021-11-21 20:17:36 +0100

    menu->zdrowie+kcal+2xBack

--- app/src/main/java/com/example/apka_v1/StartActivity.java
+++ app/src/main/java/com/example/apka_v1/StartActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.apka_v1.databinding.ActivityMainBinding;
@@ -19,6 +20,8 @@ public class StartActivity extends Activity {
         setContentView(R.layout.activity_start);
         GoBack();
         GoMapa();
+        GoToKcal();
+        GoToZdrowie();
     }
     private void GoBack(){
         ImageButton StartButton = (ImageButton) findViewById(R.id.BackButton);
@@ -30,12 +33,30 @@ public class StartActivity extends Activity {
         });
     }
 
-    private void GoMapa(){
+    private void GoMapa() {
         ImageButton MapaButton = (ImageButton) findViewById(R.id.MapaButton);
         MapaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(StartActivity.this, MapsActivity.class));
+            }
+        });
+    }
+        private void GoToKcal(){
+            Button KcalButton = (Button) findViewById(R.id.KcalButton);
+            KcalButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(StartActivity.this, KcalActivity.class));
+                }
+            });
+    }
+    private void GoToZdrowie(){
+        Button ZdrowieButton = (Button) findViewById(R.id.ZdrowieButton);
+        ZdrowieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, ZdrowieActivity.class));
             }
         });
     }
