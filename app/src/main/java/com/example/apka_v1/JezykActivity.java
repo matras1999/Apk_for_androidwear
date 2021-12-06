87d4f50e5447cea064428e71264125726f9391d0
Szymon Matras <matras1999@gmail.com>
2021-12-06 14:16:51 +0100

    dziala

--- app/src/main/java/com/example/apka_v1/JezykActivity.java
+++ app/src/main/java/com/example/apka_v1/JezykActivity.java
@@ -2,7 +2,10 @@ package com.example.apka_v1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,7 +15,9 @@ import android.widget.Toast;
 
 import com.example.apka_v1.databinding.ActivityMainBinding;
 
-public class  JezykActivity extends Activity {
+import java.util.Locale;
+
+public class JezykActivity extends Activity {
     private ActivityMainBinding binding;
 
     @Override
@@ -22,9 +27,30 @@ public class  JezykActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_jezyk);
     }
+
     public void goBack(View view) {
         startActivity(new Intent(JezykActivity.this, UstawieniaActivity.class));
     }
 
+    private void setAppLocale(String localeCode){
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.setLocale(new Locale(localeCode.toLowerCase()));
 
+        res.updateConfiguration(conf, dm);
+    }
+
+    public void changeOnPolish(View view) {
+        setAppLocale("pl-rPL");
+        finish();
+        startActivity(getIntent());
+    }
+
+
+    public void changeOnEnglish(View view) {
+        setAppLocale("en");
+        finish();
+        startActivity(getIntent());
+    }
 }
--- app/src/main/java/com/example/apka_v1/MainActivity.java
+++ app/src/main/java/com/example/apka_v1/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.apka_v1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.apka_v1.databinding.ActivityMainBinding;
+
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
@@ -20,10 +25,19 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        setAppLocale("pl-rPL");
     }
 
     public void goToMenuPage(View view) {
         startActivity(new Intent(MainActivity.this, MenuActivity.class));
+    }
+
+    private void setAppLocale(String localeCode){
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.setLocale(new Locale(localeCode.toLowerCase()));
+
+        res.updateConfiguration(conf, dm);
     }
 }
